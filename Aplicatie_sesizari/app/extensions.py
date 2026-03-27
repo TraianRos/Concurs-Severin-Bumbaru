d6b7a4f1accc305c40568ba8bc7d76a98a8e2b50
TraianRos <129787261+TraianRos@users.noreply.github.com>
2026-03-27 08:30:52 +0200

    Delete Aplicatie_sesizari directory

--- Aplicatie_sesizari/app/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = "web.login"
-login_manager.login_message = "Trebuie sa te autentifici pentru a continua."
